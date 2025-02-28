78f882cb269abb236d58adb9d36f5c0adbf2573d
sydulamin10 <sydul.cit.bd@gmail.com>
2025-02-28 20:50:42 +0600

    model design & admin panel work

--- student_managment_app/admin.py
+++ student_managment_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['prime_id', 'name', 'email', 'image', 'mother_name', 'father_name', 'religion']
+    search_fields = ['name', 'email', 'mother_name', 'father_name', 'religion']
+
+admin.site.register(Student, StudentAdmin)
--- student_managment_app/models.py
+++ student_managment_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+
+    GENDER = [
+    ("Male", "Male"),
+    ("Female", "Female"),
+    ("Others", "Others"),
+]
+
+    RELEGION = [
+    ("Islam", "Islam"),
+    ("Hindu", "Hindu"),
+    ("Khristian", "Khristian"),
+    ("Buddho", "Buddho"),
+    ("Atheist", "Atheist"),
+    ("Others", "Others"),
+]
+    prime_id       = models.AutoField(primary_key=True, unique=True, editable=False, blank=False, null=False)
+    name           = models.CharField(max_length=50)
+    email          = models.EmailField(max_length=50)
+    image          = models.ImageField(upload_to='images/', default='def.png', blank=True)
+    mother_name    = models.CharField(max_length=50)
+    father_name    = models.CharField(max_length=50)
+    religion       = models.CharField(choices=RELEGION, max_length=10)
+    gender         = models.CharField(choices=GENDER, max_length=10)
+    date_of_birth  = models.DateField()
+    roll           = models.IntegerField()
+    city           = models.CharField(max_length=50)
+    is_Bangladeshi = models.BooleanField(default=False)
+    created_at     = models.DateTimeField(auto_now_add=True)
+    age            = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name}"s Profile'
