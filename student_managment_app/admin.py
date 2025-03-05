1eead6ffd51349a68f659cbd6326520edb0256cf
sydulamin10 <sydul.cit.bd@gmail.com>
2025-03-05 16:01:12 +0600

    Advance Db Design

--- student_managment_app/admin.py
+++ student_managment_app/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Student
+from .models import *
 # Register your models here.
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ['prime_id', 'name', 'email', 'image', 'mother_name', 'father_name', 'religion']
-    search_fields = ['name', 'email', 'mother_name', 'father_name', 'religion']
+    list_display = ['name', 'email', 'image', 'mother_name', 'father_name', 'religion']
+    search_fields = ['name', 'email', 'mother_name', 'father_name', 'religion', 'city']
 
 admin.site.register(Student, StudentAdmin)
+admin.site.register(Hobby)
+admin.site.register(Subject)
+admin.site.register(Result)
--- student_managment_app/models.py
+++ student_managment_app/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Hobby(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Result(models.Model):
+    marks = models.FloatField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.subject.name
 class Student(models.Model):
 
     GENDER = [
@@ -17,7 +35,6 @@ class Student(models.Model):
     ("Atheist", "Atheist"),
     ("Others", "Others"),
 ]
-    prime_id       = models.AutoField(primary_key=True, unique=True, editable=False, blank=False, null=False)
     name           = models.CharField(max_length=50)
     email          = models.EmailField(max_length=50)
     image          = models.ImageField(upload_to='images/', default='def.png', blank=True)
@@ -31,6 +48,9 @@ class Student(models.Model):
     is_Bangladeshi = models.BooleanField(default=False)
     created_at     = models.DateTimeField(auto_now_add=True)
     age            = models.PositiveIntegerField()
+    result         = models.ManyToManyField(Result)
+    hobby          = models.OneToOneField(Hobby, on_delete=models.CASCADE)
+    subject        = models.ManyToManyField(Subject)
 
     def __str__(self):
         return f'{self.name}"s Profile'
