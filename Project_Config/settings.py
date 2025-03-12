e3c6c2209c395739e8139e273627a408c46517c9
sydulamin10 <sydul.cit.bd@gmail.com>
2025-03-12 15:54:02 +0600

    Implement home page with base layout and footer; configure environment variables for secret key

--- Project_Config/settings.py
+++ Project_Config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o)l#2r_x@w-i-)q8pg)k0fl$dg3r3&u&hr3_9$wa^r&ic@apzc'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
