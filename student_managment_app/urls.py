964b7201b7e3118ff855a34b186566c32e1a87b7
sydulamin10 <sydul.cit.bd@gmail.com>
2025-03-19 15:52:19 +0600

    search & delete function add

--- student_managment_app/urls.py
+++ student_managment_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . views import home
+from . views import home, delete_prof
 urlpatterns = [
     path('', home, name='home'),
+    path('delete_prof/<int:id>/', delete_prof, name='delete_prof'),
 ]
--- student_managment_app/views.py
+++ student_managment_app/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import Student
+from django.db.models import Q
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'Home/home.html')
+    all_stdudent = Student.objects.all()
+
+    if request.method == 'GET':
+        data = request.GET.get('src')
+        if data:
+            all_stdudent = Student.objects.filter(Q(name__icontains = data) | Q(email__icontains = data))
+            print(all_stdudent)
+
+    return render(request, 'index.html',{'stu': all_stdudent})
+
+
+def delete_prof(request, id):
+    all_stdudent = Student.objects.get(id=id)
+    all_stdudent.delete()
+    return redirect(home)
